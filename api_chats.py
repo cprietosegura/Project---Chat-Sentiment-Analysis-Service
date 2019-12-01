76243151c5103bc9f6aa65c3897d1153b665b97c
Celia Prieto <cprietosegura@gmail.com>
2019-12-01 21:05:09 +0100

    repor sentimientos

--- api_chats.py
+++ api_chats.py
@@ -1,14 +1,19 @@
 from bottle import route, run, get, post, request
 from pymongo import MongoClient
 from bson.json_util import dumps
+import json
 from mongo_populate import db, coll
 import random
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import pandas as pd
+#from src.sentiment import getSentimentReport
 
 #GET
 @get("/")
-def index():
+def welcome():
     """you can get all the database content"""
-    return dumps(coll.find())
+    return "Welcome to the API! \n Enjoy exploring the chats."
 
 @get("/<userName>")
 def GetUserMessages(userName):
@@ -19,6 +24,38 @@ def GetUserMessages(userName):
 def GetChats(idChat):
     """you can get all the messages of a selected chat"""
     return dumps(coll.find({'idChat' :int(idChat)},{'userName':1,'text':1,'_id':0}))
+
+def getSentimentReport(chat):
+    chat=json.loads(chat)
+    sid = SentimentIntensityAnalyzer()
+    scores=[]
+    conversation=[]
+    for text in chat:
+        conversation.append(text['userName']+': '+text['text'])
+        scores.append(sid.polarity_scores(text['text']))
+    df=pd.DataFrame(scores)
+    df=df[['neg', 'neu', 'pos']]
+    df=df.rename(columns={'neg':'Negative', 'neu':'Neutral', 'pos':'Positive'})
+    means=df.mean(axis = 0) 
+    df=pd.DataFrame(means)
+    df.columns = ['Scores']
+    df.index.name= 'Sentiment Metric'
+    print('--SENTIMENT REPORT--\n')
+    print('The following conversation has been analyse using VADER \n')
+    print(conversation)
+    return df
+
+
+@get("/chat/<idChat>/sentiment") 
+def sentimentReport(idChat):
+    """Analyze messages from chat_id"""
+    chat=dumps(coll.find({'idChat' :int(idChat)},{'userName':1,'text':1,'_id':0}))
+    print(chat)
+    print(chat[0])
+    print(len(chat))
+    report=getSentimentReport(chat)
+    return report
+
 
 
 #POST
@@ -39,6 +76,7 @@ def newUser():
 
 @post('/adduser')
 def addUserProfile():
+    """you can add an user and her message to the database"""
     idUser=coll.distinct("idUser")[-1] + 1
     name=str(request.forms.get("userName"))
     idMessage=coll.distinct("idMessage")[-1] + 1
@@ -56,7 +94,41 @@ def addUserProfile():
 
 
 
+@post('/chat/<idChat>/adduser')
+def addUserToChat(idChat):
+    """you can add an user and her message to an existing chat"""
+    idUser=coll.distinct("idUser")[-1] + 1
+    name=str(request.forms.get("userName"))
+    idMessage=coll.distinct("idMessage")[-1] + 1
+    idChat=int(idChat)
+    text=str(request.forms.get("text"))
+    document={"idUser":idUser,
+                "userName":name,
+                "idMessage":idMessage,
+                "idChat":idChat,
+                "text":text}
 
+    coll.insert_one(document)
+    print("New user added to chat{}".format(idChat))
+
+
+"""@post('/chat/<chat_id>/addmessage')
+def addUserToChat(idChat,userName):
+    #add message from an existing user to an existing chat
+    idChat=int(idChat)
+    name=str(userName)
+    #averiguar cómo decirle que idUser corresponde al username indicado
+    idUser=coll.distinct({'userName':name})
+    idMessage=coll.distinct("idMessage")[-1] + 1
+    text=str(request.forms.get("text"))
+    document={"idUser":idUser,
+                "userName":name,
+                "idMessage":idMessage,
+                "idChat":idChat,
+                "text":text}
+
+    coll.insert_one(document)
+    print("New user added to chat{}".format(idChat))"""
 
      
 run(host='0.0.0.0', port=8080)
---
+++ src/sentiment.py
@@ -0,0 +1,27 @@
+import nltk 
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import pandas as pd
+from bson.json_util import dumps
+
+def getSentimentReport(chat):
+    chat=dumps(chat)
+    print(chat)
+    sid = SentimentIntensityAnalyzer()
+    scores=[]
+    conversation=[]
+    for text in chat:
+        conversation.append(text['userName']+': '+text['text'])
+        scores.append(sid.polarity_scores(text['text']))
+    df=pd.DataFrame(scores)
+    df=df[['neg', 'neu', 'pos']]
+    df=df.rename(columns={'neg':'Negative', 'neu':'Neutral', 'pos':'Positive'})
+    means=df.mean(axis = 0) 
+    df=pd.DataFrame(means)
+    df.columns = ['Scores']
+    df.index.name= 'Sentiment Metric'
+    print('--SENTIMENT REPORT--\n')
+    print('The following conversation has been analyse using VADER \n')
+    print(conversation)
+    return df
+
+
