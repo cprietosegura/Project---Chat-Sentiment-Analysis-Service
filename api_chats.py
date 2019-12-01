e1b671ce3175e5f1faace15d59505595b3d210bb
Celia Prieto <cprietosegura@gmail.com>
2019-12-01 15:09:59 +0100

    mejorando funciones get y post

--- api_chats.py
+++ api_chats.py
@@ -4,24 +4,46 @@ from bson.json_util import dumps
 from mongo_populate import db, coll
 import random
 
-
+#GET
 @get("/")
 def index():
+    """you can get all the database content"""
     return dumps(coll.find())
 
-@get("/chat/<userName>")
+@get("/<userName>")
 def GetUserMessages(userName):
-    return dumps(coll.find({'userName':userName})) 
+    """you can get all the user messages"""
+    return dumps(coll.find({'userName':userName},{'userName':1,'text':1,'_id':0})) 
+
+@get("/chat/<idChat>/list") 
+def GetChats(idChat):
+    """you can get all the messages of a selected chat"""
+    return dumps(coll.find({'idChat' :int(idChat)},{'userName':1,'text':1,'_id':0}))
+
+
+#POST
+@post('/user/create')
+def newUser():
+    """you can add an user to the database"""
+    name = str(request.forms.get("userName"))
+    print(name)
+    new_id = coll.distinct("idUser")[-1] + 1
+    print(new_id)
+    new_user ={
+        "idUser": new_id,
+        "userName": name}
+    print(new_user)
+    coll.insert_one(new_user)
+    print("User added to collection")
+
 
 @post('/adduser')
-def addUser():
+def addUserProfile():
     idUser=coll.distinct("idUser")[-1] + 1
     name=str(request.forms.get("userName"))
-    print('name')
     idMessage=coll.distinct("idMessage")[-1] + 1
     idChat=coll.distinct("idChat")[-1] + 1
     text=str(request.forms.get("text"))
-    print('text')
     document={"idUser":idUser,
                 "userName":name,
                 "idMessage":idMessage,
@@ -32,19 +54,9 @@ def addUser():
     coll.insert_one(document)
     print("New user added to collection")
 
-"""
-@post('/user/create')
 
-def newUser():
-    name = str(request.forms.get("name"))
-    new_id = coll.distinct("idUser")[-1] + 1
-    new_user = {
-        "idUser": new_id,
-        "userName": name
-    }
-    coll.insert_one(new_user)
-    print("User added to collection")
-"""
+
+
 
      
 run(host='0.0.0.0', port=8080)
